c0f77060ea908439debe8b8580871f4c477dd99d
Craig Mayhew <craig@mayhew.io>
2019-08-07 19:16:31 +0100

    Began implementing cucumber tests - WIP

---
+++ features/organise.feature
@@ -0,0 +1,7 @@
+Feature: Organise my music.
+
+  Scenario: Organise my music, using default settings.
+    Given Music is in "unsorted" directory
+    When I consider what I am doing
+    Then I am interested in ATDD
+    And we can implement rules with regex
--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate cucumber_rust;
 
+use std::fs;
+
 pub struct MyWorld {
     // You can use this struct for mutable context in scenarios.
     foo: String
@@ -16,31 +18,38 @@ impl std::default::Default for MyWorld {
     }
 }
 
-mod example_steps {
+mod default_steps {
     // Any type that implements cucumber_rust::World + Default can be the world
     steps!(::MyWorld => {
-        given "I am trying out Cucumber" |world, step| {
-            world.foo = "Some string".to_string();
-            // Set up your context in given steps
+        given "Music is in \"unsorted\" directory" |_world, _step| {
+            //check unsorted directroy exists
+            let unsorted_path = Path::new("unittest-unsorted");
+            if !unsorted_path.exists() {
+                match std::fs::create_dir_all("unittest-unsorted") {
+                    Ok(o) => o,
+                    Err(_e) => panic!("create_dir_all failed with an error when creating unittest-unsorted folder")
+                };
+            }
+            assert_eq!(unsorted_path.exists(), true);
         };
 
-        when "I consider what I am doing" |world, step| {
+        when "I consider what I am doing" |world, _step| {
             // Take actions
             let new_string = format!("{}.", &world.foo);
             world.foo = new_string;
         };
 
-        then "I am interested in ATDD" |world, step| {
+        then "I am interested in ATDD" |world, _step| {
             // Check that the outcomes to be observed have occurred
             assert_eq!(world.foo, "Some string.");
         };
 
-        then regex r"^we can (.*) rules with regex$" |world, matches, step| {
+        then regex r"^we can (.*) rules with regex$" |_world, matches, _step| {
             // And access them as an array
             assert_eq!(matches[1], "implement");
         };
 
-        then regex r"^we can also match (\d+) (.+) types$" (usize, String) |world, num, word, step| {
+        then regex r"^we can also match (\d+) (.+) types$" (usize, String) |_world, num, word, _step| {
             // `num` will be of type usize, `word` of type String
             assert_eq!(num, 42);
             assert_eq!(word, "olika");
@@ -49,12 +58,12 @@ mod example_steps {
 }
 
 // Declares a before handler function named `a_before_fn`
-before!(a_before_fn => |scenario| {
+before!(a_before_fn => |_scenario| {
 
 });
 
 // Declares an after handler function named `an_after_fn`
-after!(an_after_fn => |scenario| {
+after!(an_after_fn => |_scenario| {
 
 });
 
@@ -67,7 +76,7 @@ cucumber! {
     features: "./features", // Path to our feature files
     world: ::MyWorld, // The world needs to be the same for steps and the main cucumber call
     steps: &[
-        example_steps::steps // the `steps!` macro creates a `steps` function in a module
+        default_steps::steps // the `steps!` macro creates a `steps` function in a module
     ],
     setup: setup, // Optional; called once before everything
     before: &[
